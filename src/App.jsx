a47d3260973a39baaaa579673ea055bed7e1904f
Your Name <you@example.com>
2023-12-11 23:28:15 -0800

    i add the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Nav from "./com/Nav";
 import Home from "./com/Home";
 import Search from "./com/Search";
 import Books from "./com/Books";
 import Tes from "./com/Tes";
-import { Routes, Route } from "react-router-dom";
+import Login from "./com/Login";
+
 function App() {
   const [ToggleSearch, setToggleSearch] = useState(false);
+
   const handleSearch = () => {
     setToggleSearch(!ToggleSearch);
   };
+
   return (
-    <div className="App dark:bg-dark ">
-      <Nav handleSearch = {handleSearch} />
-      <Home ToggleSearch={ToggleSearch} />
-      <Books/>
-      <Tes/>
+    <div className="App dark:bg-dark">
+      <Nav handleSearch={handleSearch} />
+      <Routes>
+        <Route path="/" element={<Home ToggleSearch={ToggleSearch} />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/tes" element={<Tes />} />
+        <Route path="/login" element={<Login />} />
+    
+      </Routes>
     </div>
   );
 }
