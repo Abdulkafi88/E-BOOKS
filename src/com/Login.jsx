47460dd240feeb0594b2388fa90aa352a84ab866
Abdulakfi88 <abdulkafinirig5@gmail.com>
2023-12-13 02:13:01 -0800

    i add a login page

--- src/com/Login.jsx
+++ src/com/Login.jsx
@@ -1,24 +1,61 @@
 import React from 'react'
 
-const Login = () => {
+ const Login = () => {
   return (
-   <>
-   <section>
-    <h1>sdlfjkslfkj</h1>
-    <p className='text-2xl text-center font-semibold dark:text-slate-100 my-10 underline underline-offset-8'>Login</p>
-   </section>
-   <form>
-    <div className='mb-6'>
-    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-    <input type="text" className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='shubham@example.com'/>
-    </div>
-    <div className='mb-6'>
-    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Passoword</label>
-    <input type="text" className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
-    </div>
-    <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>login</button>
-   </form>
-   </>
+    <>
+      <main>
+        <section>
+          <p className="text-2xl text-center font-semibold dark:text-slate-100 my-10 underline underline-offset-8">
+            Login
+          </p>
+        </section>
+        <form>
+          <div className="mb-6">
+            <label
+              for="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="shubham@example.com"
+              required
+              autocomplete="off"
+            ></input>
+          </div>
+          <div className="mb-6">
+            <label
+              for="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Your Passwrod
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+              autocomplete="off"
+            ></input>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Login
+          </button>
+        </form>
+        <button
+          type="submit"
+          className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Login as guest
+        </button>
+      </main>
+    </>
   )
 }
 
