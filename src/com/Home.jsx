1af12c1c6e38c13690f76152a3b6e730feaafd4f
Your Name <you@example.com>
2023-12-15 20:16:23 -0800

    i add the login

--- src/com/Home.jsx
+++ src/com/Home.jsx
@@ -11,7 +11,7 @@ const Home = ({ToggleSearch}) => {
         <div className="text my-5">
             <h1 className="text-5xl font-bold">The Ultimate eBook Store</h1>
             <p className="text-2xl my-7 px-1 dark:text-slate-300">CodeBook is the world's most popular and authoritative source for computer science ebooks. Find ratings and access to the newest books digitally.</p>
-            <Link to="#" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Explore eBooks</Link>
+            <Link to="AllBooks" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Explore eBooks</Link>
         </div>
         <div className="visual my-5 lg:max-w-xl">
             <img className="rounded-lg max-h-full" src={heroimg} alt="CodeBook Hero Section" />
--- src/com/Register.jsx
+++ src/com/Register.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { auth } from '../Firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 const Register = () => {
+  const [Email,setEmail] = useState('')
+  const [Password,setPassword] = useState("")
+  const [errMessge,setErrMessge] = useState('')
+  const handleRegister=async(e)=>{
+    e.preventDefault();
+    if(Email==='') alert('pleas type your email')
+    try{
+      const res = await createUserWithEmailAndPassword(auth,Email,Password)
+      console.log('User registered:', res.user);
+    }catch(err){
+     if(err.code==='auth/email-already-in-use'){
+      setErrMessge('email is in use already use another email')
+     }
+      console.error('Error registering user:', err);
+    }
+    
+  }
   return (
     <main>
       <section>
@@ -8,8 +26,8 @@ const Register = () => {
           Register
         </p>
       </section>
-      <form>
-        <div className="mb-6">
+      <form onSubmit={(e)=>{e.preventDefault()}}>
+        {/* <div className="mb-6">
           <label
             for="email"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -24,8 +42,9 @@ const Register = () => {
             required
             autocomplete="off"
           ></input>
-        </div>
+        </div> */}
         <div className="mb-6">
+          <p className='text-red-600 text-lg'>{errMessge}</p>
           <label
             for="email"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -39,6 +58,7 @@ const Register = () => {
             placeholder="shubham@example.com"
             required
             autocomplete="off"
+           onChange={(e)=>setEmail(e.target.value)}
           ></input>
         </div>
         <div className="mb-6">
@@ -49,14 +69,15 @@ const Register = () => {
             Your Passwrod
           </label>
           <input
-            type="email"
+            type="Passowrd"
             id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
-            autocomplete="off"
+            onChange={(e)=>setPassword(e.target.value)}
           ></input>
         </div>
         <button
+        onClick={handleRegister}
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
